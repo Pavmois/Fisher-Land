7778f35846301d62bb1a54918f13d99f29d78eca
Pavmois <58141080+Pavmois@users.noreply.github.com>
2024-06-25 21:56:28 +0300

    Settings

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,47 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <video v-if="isDesktop" autoplay muted playsinline loop id="myVideo">
+      <source src="/src/assets/desktop.mp4" type="video/mp4" />
+    </video>
+    <video v-else autoplay muted playsinline loop preload="metadata" id="myVideo">
+      <source src="/src/assets/mobile.mp4" type="video/mp4" />
+    </video>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+defineProps<{ msg: string }>()
+let isDesktop = ref(false);
+
+onMounted(() => {
+  const handleResize = () => {
+    isDesktop.value = window.innerWidth >= 600;
+  };
+
+  handleResize(); // Set initial value
+
+  window.addEventListener("resize", handleResize);
+});
+</script>
+
+<style lang="scss" scoped>
+.hello {
+  position: relative;
+  margin: auto;
+  background: rgb(43, 45, 56);
+  border-bottom: 3px solid rgb(0, 252, 126);
+}
+
+video {
+  width: 100vw;
+}
+
+h1 {
+  position: absolute;
+  left: 50%;
+  top: 10%;
+  transform: translate(-50%, -50%);
 }
 </style>
