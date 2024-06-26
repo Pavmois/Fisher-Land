c0d381f94d94a737678cc4a4bea7c4d93f437f7f
Pavmois <58141080+Pavmois@users.noreply.github.com>
2024-06-26 21:46:09 +0300

    adaptive video

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="monitor">
-    <h1>Всем привет, это мой сайт</h1>
+    <div class="desc">
+      Меня зовут Давид Фишер и я - стример
+    </div>
 
     <div class="vid">
-
+      <div class="stream">
+        <div class="back"></div>
+        <video class="video" autoplay muted playsinline loop>
+          <source src="/src/assets/1.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div class="stream-mobile">
+        <div class="back2"></div>
+        <video class="video-mobile" autoplay muted playsinline loop>
+          <source src="/src/assets/shorts.mp4" type="video/mp4" />
+        </video>
+      </div>
     </div>
 
-    <div class="stream">
-      <div class="back"></div>
-      <video class="video" autoplay muted playsinline loop>
-        <source src="/src/assets/1.mp4" type="video/mp4" />
-      </video>
-    </div>
-
-    <!-- <div class="stream-mobile">
-      <div class="back2"></div>
-      <video class="video-mobile" autoplay muted playsinline loop>
-        <source src="/src/assets/mobile.mp4" type="video/mp4" />
-      </video>
-    </div> -->
 
 
   </div>
@@ -28,14 +28,24 @@
 h1 {
   margin-bottom: 50px;
 }
+.desc {
+  margin: auto;
+  font-size: 2rem;
+}
 .monitor {
   position: relative;
+  display: flex;
+  justify-content: space-between;
   margin-top: 50px;
+}
+.vid {
+  position: relative;
+  width: 50%;
 }
 .stream {
   display: grid;
-  border: 1px solid red;
   place-items: center;
+  position: relative;
   .back {
     background-image: url('/src/assets/monitor.png');
     background-position: center;
@@ -46,10 +56,11 @@ h1 {
   }
   .video {
     position: absolute;
-    left: 50%;
+    left: 13%;
     top: 50%;
-    transform: translate(-50%, -70%);
-    height: 390px;
+    transform: translate(0, -70%);
+    width: 74%;
+    height: auto;
   }
 }
 .stream-mobile {
@@ -57,6 +68,7 @@ h1 {
   display: grid;
   place-items: center;
   height: 600px;
+  width: 320px;
   overflow: hidden;
   .back2 {
     background-image: url('/src/assets/mobile.png');
@@ -76,14 +88,32 @@ h1 {
   }
 
 }
-@media screen and (min-width: 900px) {
+@media screen and (min-width: 1900px) {
+  .stream {
+    .video {
+      left: 50%;
+      transform: translate(-50%, -70%);
+      width: 100%;
+      height: 390px;
+    }
+  }
+}
+@media screen and (min-width: 1200px) {
   .stream-mobile {
     display: none;
   }
 }
-@media screen and (max-width: 900px) {
+
+@media screen and (max-width: 1200px) {
+  .monitor {
+    flex-direction: column;
+  }
   .stream {
     display: none;
+  }
+  .vid {
+    margin: auto;
+    width: auto;
   }
 }
 </style>
