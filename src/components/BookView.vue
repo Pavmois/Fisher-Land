bca7204ae61e0547df7a80811293759e2ab26f1c
Pavmois <58141080+Pavmois@users.noreply.github.com>
2024-07-04 01:27:44 +0300

    Effects

--- src/components/BookView.vue
+++ src/components/BookView.vue
@@ -3,14 +3,10 @@
     <Divider>
       <h2>Образы</h2>
     </Divider>
-    <p class="desc">
-      Листайте книжку используя кнопки
-      <br />
-      или переворачивая страницы вручную!
-    </p>
     <Flipbook
       class="flipbook"
       :zooms="[1]"
+      :gloss="0.3"
       :pages="pages"
       v-slot="flipbook"
       ref="flipbook"
@@ -23,22 +19,22 @@
 import Divider from './Divider.vue'
 import Flipbook from "flipbook-vue";
 import img1 from "../assets/book/1.jpg";
+
 import img2 from "../assets/book/2.jpg";
 import img3 from "../assets/book/3.jpg";
+
 import img4 from "../assets/book/4.jpg";
 import img5 from "../assets/book/5.jpg";
+
 import img6 from "../assets/book/6.jpg";
 import img7 from "../assets/book/7.jpg";
+
 import img8 from "../assets/book/8.jpg";
 import img9 from "../assets/book/9.jpg";
-import img10 from "../assets/book/10.jpg";
-import img11 from "../assets/book/11.jpg";
+
 import img12 from "../assets/book/12.jpg";
 import img13 from "../assets/book/13.jpg";
-import img14 from "../assets/book/14.jpg";
-import img15 from "../assets/book/15.jpg";
-import img16 from "../assets/book/16.jpg";
-import img17 from "../assets/book/17.jpg";
+
 import img18 from "../assets/book/18.jpg";
 
 // Массив изображений для модуля Flipbook
@@ -53,14 +49,8 @@ const pages = [
   img7,
   img8,
   img9,
-  img10,
-  img11,
   img12,
   img13,
-  img14,
-  img15,
-  img16,
-  img17,
   img18,
 ];
 </script>
@@ -70,10 +60,6 @@ const pages = [
   width: 100%;
   margin: auto;
   margin-bottom: 50px;
-}
-.desc {
-  font-size: 24px;
-  color: white;
 }
 .flipbook {
   height: 700px;
@@ -99,14 +85,11 @@ const pages = [
   }
 }
 @media (max-width: 600px) {
-  .desc {
-    font-size: 18px;
+  .book-wrapper {
+    width: 90%;
   }
   .flipbook {
-    .viewport {
-      height: calc(85vh - 150px);
-      padding: 0;
-    }
+    height: 600px;
   }
 }
 </style>
